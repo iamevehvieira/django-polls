b1895d8c9fa562ecd18c5b67bb0a35d29105b4a1
iamevehvieira <vieiraamaral2609@gmail.com>
2023-05-16 10:29:09 -0300

    django

--- polls/views.py
+++ polls/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+# Create your views here.
+
+def index (request):
+    return HttpResponse('Olá... seja bem vindo à enquete')
+
+def sobre (request):
+    return HttpResponse('Olá, este é um app de enquete')
