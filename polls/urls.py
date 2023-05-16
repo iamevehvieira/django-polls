cf126df9b5431346e3ccf1d2dd419e869dd58941
iamevehvieira <vieiraamaral2609@gmail.com>
2023-05-16 11:04:25 -0300

    Inclui models Question e Choice.

--- polls/urls.py
+++ polls/urls.py
@@ -1,15 +1,6 @@
 from django.urls import path
-<<<<<<< HEAD
 from . import views
-
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('sobre/', views.sobre, name = 'sobre'),
-=======
-
-from . import views
-
-urlpatterns = [
-    path('', views.index, name='index'),
->>>>>>> ecd1c1aa8bde16f553ba2cabf0dbd9118c19ea56
 ]
--- polls/views.py
+++ polls/views.py
@@ -3,7 +3,6 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-<<<<<<< HEAD
 from django.http import HttpResponse
 
 # Create your views here.
@@ -13,9 +12,3 @@ def index (request):
 
 def sobre (request):
     return HttpResponse('Olá, este é um app de enquete')
-=======
-
-
-def index(request):
-    return HttpResponse('Olá... seja bem vindo à enquete')
->>>>>>> ecd1c1aa8bde16f553ba2cabf0dbd9118c19ea56
