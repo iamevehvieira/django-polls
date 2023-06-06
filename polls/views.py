103b77707c5a1f452a3b74717a0054164bbf7f25
iamevehvieira <vieiraamaral2609@gmail.com>
2023-06-06 11:05:26 -0300

    view e rota que exibe uma pergunta pelo id

--- polls/views.py
+++ polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
+from polls.models import Question, Choice
 
 from django.http import HttpResponse
 
@@ -12,3 +13,13 @@ def index (request):
 
 def sobre (request):
     return HttpResponse('Olá, este é um app de enquete')
+
+def exibe_questao(request, question_id):
+    question = Question.objects.get(id=question_id)
+
+    if question is not None:
+        #questao.question_text
+        return HttpResponse(f'{question.question_text}')
+    
+    return HttpResponse('Não existe questão a exibir')
+
