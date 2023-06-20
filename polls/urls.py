7369ed9f1cbbcf333f3b61da00497882d1dd2cf4
iamevehvieira <vieiraamaral2609@gmail.com>
2023-06-20 10:52:34 -0300

    Inclui página com as últimas perguntas.

--- polls/urls.py
+++ polls/urls.py
@@ -3,5 +3,6 @@ from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
     path('sobre/', views.sobre, name = 'sobre'),
-    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao')
+    path('pergunta/<int:question_id>', views.exibe_questao, name='exibe_questao'),
+    path('perguntas', views.ultimas_perguntas, name='ultimas_perguntas'),
 ]
