04fcdc3971f68f804f5c1e04c11f86e3a7ad353d
isnteve <vieiraamaral2609@gmail.com>
2023-09-20 09:04:28 -0300

    Inclui flash messages.

--- polls/views.py
+++ polls/views.py
@@ -1,5 +1,6 @@
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
+from django.contrib import messages
 from django.urls import reverse_lazy
 from django.shortcuts import render
 from django.http import HttpResponse
@@ -57,7 +58,11 @@ def get_context_data(self, **kwargs):
 class QuestionDeleteView(DeleteView):
     model = Question
     template_name = 'polls/question_confirm_delete_form.html'
-    success_url = reverse_lazy('polls_list')
+    success_url = reverse_lazy('polls_all')
+    success_message = 'Pergunta excluída com sucesso.'
+def form_valid(self, request, *args, **kwargs):
+    messages.success(self.request, self.success_message)
+    return super(QuestionDeleteView, self).form_valid(request, *args, **kwargs)
 
 class QuestionDetailView(DetailView):
     model = Question
