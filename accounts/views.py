d2a89487fa69f59a037a3a5506570936ccc70ca6
isnteve <vieiraamaral2609@gmail.com>
2023-12-05 10:40:14 -0300

    Enquetes Votadas pelo Usuário

--- accounts/views.py
+++ accounts/views.py
@@ -1,15 +1,20 @@
+from typing import Any
 from django.shortcuts import render
 
 # Create your views here.
-
 from django.views.generic.edit import CreateView, UpdateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
 from django.contrib.auth.hashers import make_password # para criptografar a senha
 from django.contrib import messages
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.views.generic import TemplateView
+
 from django.contrib.auth import get_user_model
 User = get_user_model() # obtém o model padrão para usuários do Django
+
 from accounts.forms import AccountSignupForm # importa o form de registro
+
+from polls.models import QuestionUser
 class AccountCreateView(CreateView):
     model = User # conecta o model a view
     template_name = 'registration/signup_form.html' # template para o form HTML
@@ -42,3 +47,13 @@ class AccountUpdateView(LoginRequiredMixin, UpdateView):
     def form_valid(self, form): # executa quando os dados estiverem válidos
         messages.success(self.request, self.success_message)
         return super(AccountUpdateView, self).form_valid(form)
+    
+class AccountTemplateView(LoginRequiredMixin, TemplateView):
+    template_name = 'accounts/user_detail.html'
+    context_object_name = 'user'
+
+    def get_context_data(self, **kwargs):
+        context = super(AccountTemplateView, self).get_context_data(**kwargs)
+        voted = QuestionUser.objects.filter(user=self.request.user)
+        context['questions_voted'] = voted
+        return context
